6d9fb65a6e44c65946d528410f50373aa85ae0ce
DixzzXD88 <rf3056098@gmail.com>
2025-11-06 20:10:05 +0700

    Add files via upload

---
+++ plugin/downloadermenu.js
@@ -0,0 +1,73 @@
+const moment = require("moment-timezone")
+require("moment/locale/id")
+moment.locale("id")
+
+module.exports = async (sock, msg, args, { isOwner }) => {
+  try {
+    const sender = msg.pushName || "User"
+    const nomor = msg.key.remoteJid
+      ? msg.key.remoteJid.split("@")[0]
+      : "Tidak diketahui"
+
+    await sock.sendMessage(msg.from, {
+      react: { text: "âœ¨", key: msg.key }
+    })
+
+    const tanggal = moment().tz("Asia/Jakarta").format("dddd, DD MMMM YYYY")
+    const jam = moment().tz("Asia/Jakarta").hour()
+    let emojiWaktu = "ðŸ•’"
+    if (jam >= 5 && jam < 11) emojiWaktu = "ðŸŒ…"
+    else if (jam >= 11 && jam < 15) emojiWaktu = "â˜€ï¸"
+    else if (jam >= 15 && jam < 18) emojiWaktu = "ðŸŒ‡"
+    else if (jam >= 18 && jam < 22) emojiWaktu = "ðŸŒŒ"
+
+    const waktu = moment().tz("Asia/Jakarta").format("HH:mm") + " " + emojiWaktu
+
+    const teks = `
+=======ã€Ž Info Bot ã€=======
+
+ã„” Nama: ${sender}
+ã„” Nomor: ${nomor}
+
+ã„” Halo kak: ${sender}
+ã„” Tanggal: ${tanggal}
+ã„” Waktu: ${waktu}
+
+ã„” Nama Bot: DixzzXD
+ã„” Language : JavaScript
+ã„” Type : Plugin
+ã„” Developer : Dixzz
+
+â•”â”€â”€ã€Ž Random ã€
+â”‚ãƒ„ .tiktok
+â”‚ãƒ„ .tiktokslide
+â”‚ãƒ„ .tiktokaudio
+â”‚ãƒ„ .ytmp3
+â”‚ãƒ„ .ytmp4
+â”‚ãƒ„ .instagram
+â”‚ãƒ„ .spotify
+â”‚ãƒ„ .videy
+â•šâ”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â˜‰
+`
+
+    await sock.sendMessage(
+      msg.from,
+      {
+        image: { url: "https://files.catbox.moe/1fsgrj.jpg" },
+        caption: teks
+      },
+      { quoted: msg }
+    )
+
+    await sock.sendMessage(msg.from, {
+      react: { text: "âœ…", key: msg.key }
+    })
+  } catch (err) {
+    console.error("Error di plugin randommenu:", err)
+    await sock.sendMessage(
+      msg.from,
+      { text: "âŒ Terjadi error di plugin randommenu" },
+      { quoted: msg }
+    )
+  }
+}
